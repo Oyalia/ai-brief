90072c4987e115b8a26cf21a01a200aff066588d
Nazarii <ipz223_sno@student.ztu.edu.ua>
2026-04-27 02:05:50 +0300

    feat:initial commit fix 2

--- test-db.ts
+++ test-db.ts
@@ -7,8 +7,7 @@ import "dotenv/config";
 neonConfig.webSocketConstructor = ws;
 
 const connectionString = `${process.env.DATABASE_URL}`;
-const pool = new Pool({ connectionString });
-const adapter = new PrismaNeon(pool);
+const adapter = new PrismaNeon({ connectionString });
 const prisma = new PrismaClient({ adapter });
 
 async function main() {
